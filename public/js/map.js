f46d20e0f8725323dc6e269e9ef660b2c9a09444
Numan <9256fa@gmail.com>
2024-10-31 21:22:46 +0530

    Add map html and Schema: Accessed map token

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,3 @@
-
 mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
@@ -7,5 +6,11 @@ const map = new mapboxgl.Map({
   zoom: 9, // starting zoom
   // center: listing.geometry.coordinates // starting position
   //longitude then latitude
-  center: [77.209, 28.6139], // starting position[lng,lat]
+  center: coordinates, // starting position[lng,lat]
 });
+
+// console.log(coordinates);
+// Map Marker
+const marker = new mapboxgl.Marker()
+  .setLngLat(coordinates) //listing.geometry.coordinates
+  .addTo(map);
